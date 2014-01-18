c71c69459108c714f255cd863864bcc3e6cd7cde
retani <holgheiss@gmail.com>
2014-01-18 16:04:22 +0100

    4 charts on projection. style tv and enable page switching

--- app/assets/javascripts/local.js
+++ app/assets/javascripts/local.js
@@ -124,9 +124,20 @@ $(document).ready(function() {
             var t=this;
             setTimeout(function() {
                 new Chart ("chart-"+$(t).data("symbol"), "/stocks/chart_data/"+$(t).data("id"), $(t).data("title"), $(t).data("min"), $(t).data("max"));
-            }, Math.round($(this).data("id")*400));
+            }, Math.round($(this).data("id")*100));
         });
     }
+
+    // keyboard
+
+    $( "body" ).keypress(function( event ) {
+      number = event.keyCode-48;
+      if (number < 1 || number > 9) return false;
+      page = $("#page" + number + ".page");
+      if (page.length != 1) return false;
+      $(".page").hide();
+      $("#page" + number + ".page").show();
+    });    
 
     // 100% height
     windowHeight = $(window).height();
@@ -311,7 +322,7 @@ function Chart(canvas_id, url, title, min, max) {
 
 	this.chart = new CanvasJS.Chart(canvas_id,{
 		backgroundColor: "transparent",
-		width: $("#"+canvas_id).parent().width(),
+		/* width: $("#"+canvas_id).parent().width(), */
 		culture: "de",
 		creditHref: "",
 		creditText: "",	
@@ -354,7 +365,7 @@ function Chart(canvas_id, url, title, min, max) {
         }*/]
 	});
 	 
-	this.chart.render();
+	//this.chart.render();
 	
 	setInterval(function(){
 		
