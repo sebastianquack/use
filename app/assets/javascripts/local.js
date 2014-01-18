60cb602f752ba4f14303592929ccc0a05762d266
retani <holgheiss@gmail.com>
2014-01-18 16:31:17 +0100

    javascript keyboard page switch

--- app/assets/javascripts/local.js
+++ app/assets/javascripts/local.js
@@ -124,20 +124,22 @@ $(document).ready(function() {
             var t=this;
             setTimeout(function() {
                 new Chart ("chart-"+$(t).data("symbol"), "/stocks/chart_data/"+$(t).data("id"), $(t).data("title"), $(t).data("min"), $(t).data("max"));
-            }, Math.round($(this).data("id")*100));
+            }, Math.round($(this).data("id")*150));
         });
     }
 
-    // keyboard
+    // keyboard page switch
 
-    $( "body" ).keypress(function( event ) {
-      number = event.keyCode-48;
-      if (number < 1 || number > 9) return false;
-      page = $("#page" + number + ".page");
-      if (page.length != 1) return false;
-      $(".page").hide();
-      $("#page" + number + ".page").show();
-    });    
+    if ($(".page"). length >= 1) {
+        $("body").keypress(function( event ) {
+          number = event.keyCode-48;
+          if (number < 1 || number > 9) return false;
+          page = $("#page" + number + ".page");
+          if (page.length != 1) return false;
+          $(".page").hide();
+          $("#page" + number + ".page").show();
+        });
+    }
 
     // 100% height
     windowHeight = $(window).height();
